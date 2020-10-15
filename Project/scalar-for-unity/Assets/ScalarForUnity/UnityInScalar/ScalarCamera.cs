39f969898d5e51c87ceb49e54ef005441a50a86b
Erik Loyer <erik@song.nu>
2020-10-15 15:39:12 -0700

    Update ScalarCamera to use new 3D annotation format

--- Project/scalar-for-unity/Assets/ScalarForUnity/UnityInScalar/ScalarCamera.cs
+++ Project/scalar-for-unity/Assets/ScalarForUnity/UnityInScalar/ScalarCamera.cs
@@ -1,13 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using SimpleJSON;
 
 namespace ANVC.Scalar
 {
     public class ScalarCamera : MonoBehaviour
     {
-
-        public Transform target;
 
         private Camera _camera;
 
@@ -17,29 +16,15 @@ namespace ANVC.Scalar
             _camera = GetComponent<Camera>();
         }
 
-        public void SetTransform(string transformStr)
+        public void SetTransform(string point_3d)
         {
-            // data is comma-delimited
-            string[] temp = transformStr.Split(',');
-
-            // first three values are x, y, and z position
-            Vector3 position = new Vector3(float.Parse(temp[0]), float.Parse(temp[1]), float.Parse(temp[2]));
-            transform.position = position;
-            Quaternion rotation;
-
-            // second three values are heading, tilt, and field of view
-            if (temp.Length == 6)
-            {
-                rotation = Quaternion.Euler(float.Parse(temp[4]), float.Parse(temp[3]), 0);
-                transform.rotation = rotation;
-                _camera.fieldOfView = float.Parse(temp[5]);
-            }
-
-            // otherwise look at the target (if present)
-            else if (target != null)
-            {
-                transform.LookAt(target);
-            }
+            JSONNode node = JSON.Parse(point_3d);
+            Vector3 targetPosition = new Vector3(node["targetX"], node["targetY"], node["targetZ"]);
+            Vector3 cameraPosition = new Vector3(node["cameraX"], node["cameraY"], node["cameraZ"]);
+            transform.position = cameraPosition;
+            transform.LookAt(targetPosition);
+            transform.RotateAround(cameraPosition, targetPosition - cameraPosition, node["roll"]);
+            _camera.fieldOfView = node["fieldOfView"];
         }
     }
 }
